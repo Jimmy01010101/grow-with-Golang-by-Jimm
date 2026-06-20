a9d3778672fef14c79c5efe1b4ab795742ebcb28
jimmy01010101 <jimmy01010101@users.noreply.github.com>
2026-06-20 22:23:17 +0700

    add file jwt on folder auth digunakan untuk generate token yang berisi informasi user dan juga berfungsi untuk memvalidasi kecocokan token dengan data user lalu mengembalikan data user yang tersimpan didalam token, jika tidak cocok validasi akan gagal yang bisa disebabkan oleh kesalahan token atau expired token

---
+++ auth/jwt.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("pembelajaran-testing-aslinya-jangan-pake-hardkode")
+
+func GenerateToken(userID int, username string) (string, error) {
+	claims := jwt.MapClaims{
+		"sub":      userID,
+		"username": username,
+		"iat":      time.Now().Unix(),
+		"exp":      time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
+}
+
+func ParseToken(tokenString string) (string, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("metode signing tidak valid")
+		}
+		return secretKey, nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return "", errors.New("token tidak valid")
+	}
+
+	username, _ := claims["username"].(string)
+	return username, nil
+}
